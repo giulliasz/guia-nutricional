ec9a73bbf809ae4a84e087c33be251de3802ee8b
giulliasz <giulliateixeira_@hotmail.com>
2026-04-11 13:30:32 -0300

    Update questionario.js

--- guia-nutricional/pages/questionario.js
+++ guia-nutricional/pages/questionario.js
@@ -194,8 +194,8 @@ Crie um GUIA NUTRICIONAL E DE TREINO COMPLETO E PERSONALIZADO em português bras
       .replace(/^# (.*$)/gm, '<h1>$1</h1>')
       .replace(/^## (.*$)/gm, '<h2>$1</h2>')
       .replace(/^### (.*$)/gm, '<h3>$1</h3>')
-      .replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>')
-      .replace(/\*(.*?)\*/g, '<em>$1</em>')
+      .replace(/[*][*](.*?)[*][*]/g, '<strong>$1</strong>')
+      .replace(/[*](.*?)[*]/g, '<em>$1</em>')
       .replace(/^- (.*$)/gm, '<li>$1</li>')
       .replace(/^---$/gm, '<hr>')
       .replace(/\n/g, '<br>');
